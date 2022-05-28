78ba3a46d8445f80eace612e92afa74fa5683bcd
fismael21 <fernandoismaelvirgo@gmail.com>
2022-05-28 10:08:04 -0500

    Actualizacion

--- script.js
+++ script.js
@@ -20,28 +20,60 @@ diceEl.classList.add("hidden");
 const scores = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
+let playing = true;
+
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+
+  player0El.classList.toggle("player--active");
+  player1El.classList.toggle("player--active");
+};
 
 btnRoll.addEventListener("click", function () {
-  //1. GENERATING A RANDOM DICE ROLL
-  const dice = Math.trunc(Math.random() * 6) + 1;
-  console.log(dice);
-  //2. DISPLAY DICE
-  diceEl.classList.remove("hidden");
-  diceEl.src = `img/dice-${dice}.png`;
+  if (playing) {
+    //TODO: GENERATE A RANDOM DICE ROLL
+    const dice = Math.trunc(Math.random() * 6) + 1;
+    console.log(dice);
+    //TODO: DISPLAY DICE
+    diceEl.classList.remove("hidden");
+    diceEl.src = `img/dice-${dice}.png`;
 
-  //3. CHECK FOR ROLLED 1: IF TRUE, SWITCH TO NEXT PLAYER
-  if (dice !== 1) {
-    //ADD DICE TO THE CURRENT SCORE
-    currentScore += dice;
+    //TODO: CHECK FOR ROLLED 1: IF TRUE, SWITCH TO NEXT PLAYER
+    if (dice !== 1) {
+      //TODO: ADD DICE TO THE CURRENT SCORE
+      currentScore += dice;
 
-    document.getElementById(`current--${activePlayer}`).textContent =
-      currentScore;
-  } else {
-    document.getElementById(`current--${activePlayer}`).textContent = 0;
-    currentScore = 0;
-    activePlayer = activePlayer === 0 ? 1 : 0;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+    } else {
+      switchPlayer();
+    }
+  }
+});
 
-    player0El.classList.toggle("player--active");
-    player1El.classList.toggle("player--active");
+btnHold.addEventListener("click", function () {
+  if (playing) {
+    diceEl.classList.add("hidden");
+    //TODO: ADD CURRENT SCORE TO ACTIVE PLAYER'S SCORE
+    scores[activePlayer] += currentScore;
+    console.log(scores[activePlayer]);
+    document.querySelector(`#score--${activePlayer}`).textContent =
+      scores[activePlayer];
+    //TODO: CHECK IF PLAYER'S SCORE IS >= 100. IF SO, FINISH THE GAME
+    if (scores[activePlayer] >= 15) {
+      playing = false;
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add("player--winner");
+
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove("player--active");
+    } else {
+      //TODO: SWITCH TO THE NEXT PLAYER
+      switchPlayer();
+    }
   }
 });
