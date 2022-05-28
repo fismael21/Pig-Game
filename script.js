15ea2b08aaa4445a491732857d157cbbdea364db
fismael21 <fernandoismaelvirgo@gmail.com>
2022-05-28 05:57:09 -0500

    First commit

---
+++ script.js
@@ -0,0 +1,11 @@
+"use strict";
+//SELECTING ELEMENTS
+//const score0 = document.getElementById("score--0");
+const score0El = document.querySelector("#score--0");
+const score1El = document.querySelector("#score--1");
+const diceEl = document.querySelector(".dice");
+
+//STARTING CONDITIONS
+score0El.textContent = 0;
+score1El.textContent = 0;
+//diceEl.classList.add("hidden");
